d425ffe9b64ed790c71c8f51704086b94af95a86
Laura <almasilaura@outlook.com>
2020-12-14 21:57:24 +0000

    TEST 'test_transaction_has_tag' in 'TestTransaction' PASSED

--- tests/transaction_test.py
+++ tests/transaction_test.py
@@ -5,4 +5,8 @@ class TestTransaction(unittest.TestCase):
 
     def setUp(self):
 
-        self.transaction = Transaction("Holdiays", "Overlook Hotel", 600)
+        self.transaction = Transaction("Holdiays", "Overlook Hotel", 1912)
+
+    def test_transaction_has_tag(self):
+        self.assertEqual("Holdiays", self.transaction.tag)
+
