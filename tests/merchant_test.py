90095bda2f9876882e0707c2a2ce9806520549f7
Laura <almasilaura@outlook.com>
2020-12-14 15:55:04 +0000

    TEST 'test_merchant_has_name' in 'TestMerchant' PASSED

--- tests/merchant_test.py
+++ tests/merchant_test.py
@@ -6,3 +6,6 @@ class TestMerchant(unittest.TestCase):
     def setUp(self):
 
         self.merchant = Merchant("Overlook Hotel")
+
+    def test_merchant_has_name(self):
+        self.assertEqual("Overlook Hotel", self.merchant.name)
