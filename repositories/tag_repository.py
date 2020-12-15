8ff8da5ca0049a424ca19d25b5bc3b29ca9df71d
Laura <almasilaura@outlook.com>
2020-12-15 23:50:37 +0000

    ADDED function 'select_all' to 'tag_repository'

--- repositories/tag_repository.py
+++ repositories/tag_repository.py
@@ -7,3 +7,13 @@ def save(tag):
     results = run_sql(sql, values)
     tag.id  = results[0]['id']
     return tag
+
+def select_all(tag):
+    tags = []
+    sql = "SELECT * FROM tags"
+    results = run_sql(sql)
+
+    for row in results:
+        tag = Tag(row['name'], row['id'])
+        tags.append(tag)
+    return tags
