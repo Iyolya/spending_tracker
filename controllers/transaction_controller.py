4f75e1cd17801d4d17b5a0dd2f1c1918ea29082e
Laura <almasilaura@outlook.com>
2020-12-16 13:19:55 +0000

    ADDED route '/transactions' in 'transaction_controller'

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -6,3 +6,10 @@ import repositories.tag_repository as tag_repository
 import repositories.transaction_repository as transaction_repository
 
 transactions_blueprint = Blueprint("transactions", __name__)
+
+@transactions_blueprint.route("/transactions")
+def transactions():
+    transactions = transaction_repository.select_all()
+    total = transaction_repository.total()
+    return render_template("transactions/index.html", transactions=transactions, total=total)
+
