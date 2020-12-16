04a2d9eb2f27c0f48aa63c3208e427a0e34b9efa
Laura <almasilaura@outlook.com>
2020-12-16 08:13:13 +0000

    CREATED 3 objects of class 'Transaction'

--- console.py
+++ console.py
@@ -1,9 +1,11 @@
 import pdb
 from models.merchant import Merchant
 from models.tag import Tag
+from models.transaction import Transaction
 
 import repositories.merchant_repository as merchant_repository
 import repositories.tag_repository as tag_repository
+import repositories.transaction_repository as transaction_repository
 
 merchant1 = Merchant("Overlook Hotel")
 merchant_repository.save(merchant1)
@@ -23,6 +25,16 @@ tag_repository.save(tag2)
 
 tag3 = Tag("Eating out")
 tag_repository.save(tag3)
+
+
+transaction1 = Transaction("Holidays", "Overlook Hotel", 1313)
+transaction_repository.save(transaction1)
+
+transaction2 = Transaction("Self-development", "Stars Hollow Library", 30)
+transaction_repository.save(transaction2)
+
+transaction3 = Transaction("Eating out", "Luke's Diner", 65)
+transaction_repository.save(transaction3)
 
 
 pdb.set_trace
